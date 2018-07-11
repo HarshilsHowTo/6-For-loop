90b2052fb51c7a972e6e10530b8161f4d2b33ae0
Harshil <harshils.how.to@gmail.com>
2018-07-11 22:19:31 +0100

    Video Code

---
+++ ForLoops.java
@@ -0,0 +1,10 @@
+public class ForLoops {
+
+    public static void main(String[] args) {
+        for(int count = 40; count > 10; count -= 2){
+            System.out.println(count);
+        }
+        System.out.println("Loop end");
+    }
+
+}
